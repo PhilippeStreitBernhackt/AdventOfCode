9cc643b1109edc60514d599709638ca2122252db
Philippe Streit <112244706+PhilippeStreitBernhackt@users.noreply.github.com>
2023-12-04 21:54:12 +0100

    day4

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
@@ -23,14 +24,19 @@ class Program
             }
 
             result = ProcessLine1(input);
-
             Console.WriteLine("Result Puzzle 1: " + result.ToString());
 
-            /*
             // Puzzle 2
+            char[][] map = new char[input.Count][];
+            for(int i = 1; i <= input.Count; i++)
+            {
+                char[] a = input[i].ToCharArray();
+                map[i-1] = a;
+            }
 
-
-            */
+            int result2 = 0;
+            result2 += ProcessLine2(map, input);
+            Console.WriteLine("Result Puzzle 2: " + result2.ToString());
 
         }
         catch (IOException e)
@@ -72,13 +78,63 @@ class Program
         return check.Any(ch => ! char.IsLetterOrDigit(ch));
     }
 
-    private static int ProcessLine2(string input)
+    private static int FindNumber(string s, string possible, ref List<int> numbers)
     {
+        Regex regexNum = new Regex("\\d+");
+        MatchCollection matches = regexNum.Matches(s);
+        if(matches > 0)
+        {
+                foreach (Match match in matches)
+                {
+                    switch(match.Length)
+                    {
+                        case 1:
+                        case 2:
+                        case default:
+                    }
+                }
+        }
+
+        return matches.Count;
+    }
+
+    private static int ProcessLine2(char[][] map, Dictionary<int, string> input)
+    {
+        for(int i = 1; i <= input.Count; i++)
+        {
+            Regex regex = new Regex("\\*");
+            MatchCollection matches = regex.Matches(input[i]);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    List<int> numbers = new List<int>();
+                    int foundL1 = FindNumber(input[i-1].Substring(match.Index-1,3), input[i-1].Substring(match.Index-3,7), ref numbers);
+                    int foundL2 = FindNumber(input[i].Substring(match.Index-1,3), input[i].Substring(match.Index-3,7), ref numbers);
+                    int foundL3 = FindNumber(input[i+1].Substring(match.Index-1,3), input[i+1].Substring(match.Index-3,7), ref numbers);
+
+                    if (foundL1+foundL2+foundL3==3)
+                    {
+
+                        Console.WriteLine(foundL1.ToString() + foundL2.ToString() + foundL3.ToString());
+
+                        for(int y = i - 2; y <= i; y++)
+                        {
+                            for(int x = match.Index - 3; x <= match.Index + 3; x++)
+                            {
+                                //Console.Write(map[y][x]);
+                            }
+                            //Console.WriteLine();
+                        }
+                        //Console.WriteLine("------");
+
+                    }
+                }
+            }
+        }
 
         return 0;
 
     }
-
-
 
 }
---
+++ AdventOfCode/Day4/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+class Program
+{
+
+    public static void Main()
+    {
+        try
+        {
+
+            // Puzzle 1
+            using var sr = new StreamReader("..\\..\\..\\inputPuzzle1.txt");
+            String ?line;
+            List<string> cards = new List<string>();
+            Dictionary<int,int> repeat = new Dictionary<int, int>();
+            int result = 0;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                result += ProcessLine1(line);
+                cards.Add(line);
+            }
+
+            Console.WriteLine("Result Puzzle 1: " + result.ToString());
+
+            int result2 = 0;
+            foreach(string s in cards)
+            {
+                result2 += ProcessLine2(s, ref repeat);
+            }
+
+            Console.WriteLine("Result Puzzle 2: " + result2.ToString());
+
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static int ProcessLine1(string line)
+    {
+        int result = 0;
+
+        line = line.Replace("  ", " ");
+
+        var game = line.Split(new string[] { ":" }, StringSplitOptions.None);
+        var cards = game[1].Trim().Split(new string[] { "|" }, StringSplitOptions.None);
+        var winning = cards[0].Trim().Split(new string[] { " " }, StringSplitOptions.None);
+        var numbers = cards[1].Trim().Split(new string[] { " " }, StringSplitOptions.None);
+
+        List<int> winningNumbers = new List<int>();
+        foreach(string s in winning) winningNumbers.Add(int.Parse(s));
+        foreach(string s in numbers)
+        {
+            if(winningNumbers.Contains(int.Parse(s)))
+            {
+                result = (result == 0) ? 1 : result*2;
+            }
+        } 
+
+        return result;       
+    }
+
+    private static int ProcessLine2(string line, ref Dictionary<int,int> repeat)
+    {
+        int counter = 0;
+
+        line = line.Replace("   ", " ");
+        line = line.Replace("  ", " ");
+
+        var game = line.Split(new string[] { ":" }, StringSplitOptions.None);
+        int gameId = int.Parse(game[0].Trim().Split(new string[] { " " }, StringSplitOptions.None)[1]);
+        var cards = game[1].Trim().Split(new string[] { "|" }, StringSplitOptions.None);
+        var winning = cards[0].Trim().Split(new string[] { " " }, StringSplitOptions.None);
+        var numbers = cards[1].Trim().Split(new string[] { " " }, StringSplitOptions.None);
+
+        List<int> winningNumbers = new List<int>();
+        foreach(string s in winning) winningNumbers.Add(int.Parse(s));
+
+        // Count winning numbers
+        foreach(string s in numbers)
+        {
+            if(winningNumbers.Contains(int.Parse(s))) counter += 1;
+        } 
+
+        // set copies of cards for each winning number
+        int repeater = 0;
+        if(repeat.ContainsKey(gameId)) repeater = repeat[gameId];
+        for(int i = 1; i <= counter; i++)
+        {
+            if(repeat.ContainsKey(gameId+i)) { repeat[gameId+i] += 1 + repeater;} else { repeat.Add(gameId+i, 1 + repeater);}
+        }
+
+        return repeater + 1 ;       
+
+    }
+
+
+
+}
